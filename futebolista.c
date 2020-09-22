627105e50c827d3f12d305642893c7868df68ca7
BrHollow <71444366+BrHollow@users.noreply.github.com>
2020-09-22 11:28:33 -0300

    Create futebolista.c

---
+++ futebolista.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+typedef struct Jogador{
+    int id;
+    char nome[20];
+    char timee[20];
+    int camisa;
+    char pe[20];
+    char posição[20];
+    float salário;
+    int idade;
+    int gols;
+}Jogador;
